6dc87e71e0feff8c499c85a623eb1a80053a7810
Felipe Coury <felipe.coury@gmail.com>
2026-03-01 21:25:19 -0300

    chore(homebrew): update tsql to 0.5.0

--- Formula/tsql.rb
+++ Formula/tsql.rb
@@ -1,22 +1,22 @@
 class Tsql < Formula
   desc "A modern, keyboard-first PostgreSQL CLI"
   homepage "https://github.com/fcoury/tsql"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/fcoury/tsql/releases/download/v#{version}/tsql-aarch64-apple-darwin.tar.gz"
-      sha256 "2d5fb839b878d3f57e7af213a41883b87c5b1ca5fa61d4097dd88c56f9a88018"
+      sha256 "9670201266f970ef14b12aaa626b7051de800b6ca9685c51d6e1463e94940eac"
     else
       url "https://github.com/fcoury/tsql/releases/download/v#{version}/tsql-x86_64-apple-darwin.tar.gz"
-      sha256 "e6fb6b5556c5e3358514ca626559047e95b1995df3302857bfe5c72ba497a6fe"
+      sha256 "21bb964bab3f262d4fd3d4e567402546a65a4c03eff65298a2f06d9035ddf8f8"
     end
   end
 
   on_linux do
     url "https://github.com/fcoury/tsql/releases/download/v#{version}/tsql-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "054d3272ff8b43b79944333c59f5e3028fc35340572c048a554518db56637ae6"
+    sha256 "64f6f78946a71fcc3c5adce8d86a6adfab069de1346b98688309a8e3bd4f2a87"
   end
 
   def install
