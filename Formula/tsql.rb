45b359507f41fe7081021cd287a75720a90deee8
Felipe Coury <felipe.coury@gmail.com>
2026-03-01 21:26:32 -0300

    fix(homebrew): modernize tsql formula layout and metadata

--- Formula/tsql.rb
+++ Formula/tsql.rb
@@ -1,21 +1,18 @@
 class Tsql < Formula
-  desc "A modern, keyboard-first PostgreSQL CLI"
+  desc "Modern, keyboard-first PostgreSQL CLI"
   homepage "https://github.com/fcoury/tsql"
-  version "0.5.0"
   license "MIT"
 
-  on_macos do
+  if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/fcoury/tsql/releases/download/v#{version}/tsql-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/fcoury/tsql/releases/download/v0.5.0/tsql-aarch64-apple-darwin.tar.gz"
       sha256 "9670201266f970ef14b12aaa626b7051de800b6ca9685c51d6e1463e94940eac"
     else
-      url "https://github.com/fcoury/tsql/releases/download/v#{version}/tsql-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/fcoury/tsql/releases/download/v0.5.0/tsql-x86_64-apple-darwin.tar.gz"
       sha256 "21bb964bab3f262d4fd3d4e567402546a65a4c03eff65298a2f06d9035ddf8f8"
     end
-  end
-
-  on_linux do
-    url "https://github.com/fcoury/tsql/releases/download/v#{version}/tsql-x86_64-unknown-linux-gnu.tar.gz"
+  elsif OS.linux?
+    url "https://github.com/fcoury/tsql/releases/download/v0.5.0/tsql-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "64f6f78946a71fcc3c5adce8d86a6adfab069de1346b98688309a8e3bd4f2a87"
   end
 
@@ -24,6 +21,6 @@ class Tsql < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/tsql --version")
+    assert_match "0.5.0", shell_output("#{bin}/tsql --version")
   end
 end
